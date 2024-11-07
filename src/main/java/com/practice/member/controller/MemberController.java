8269f665ee97edbc46d049696171b70e700f310e
Minsu Kim <gts2044@gmail.com>
2024-11-08 01:41:01 +0900

    회원정보 상세조회, 수정, 삭제, 로그아웃

--- src/main/java/com/practice/member/controller/MemberController.java
+++ src/main/java/com/practice/member/controller/MemberController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.practice.member.dto.MemberDTO;
@@ -53,5 +54,38 @@ public class MemberController {
 		List<MemberDTO> memberDTOList = memberService.findAll();
 		model.addAttribute("memberList", memberDTOList);
 		return "list";
+	}
+
+	@GetMapping("/member/{id}")
+	public String findById(@PathVariable("id") Long id, Model model) {
+		MemberDTO memberDTO = memberService.findById(id);
+		model.addAttribute("member", memberDTO);
+		return "detail";
+	}
+
+	@GetMapping("/member/update")
+	public String updateForm(HttpSession session, Model model) {
+		String myEmail = (String) session.getAttribute("loginEmail");
+		MemberDTO memberDTO = memberService.updateForm(myEmail);
+		model.addAttribute("updateMember", memberDTO);
+		return "update";
+	}
+
+	@PostMapping("/member/update")
+	public String update(@ModelAttribute MemberDTO memberDTO) {
+		memberService.update(memberDTO);
+		return "redirect:/member/" + memberDTO.getId();
+	}
+
+	@GetMapping("member/delete/{id}")
+	public String deleteById(@PathVariable("id") Long id) {
+		memberService.deleteById(id);
+		return "redirect:/member/";
+	}
+
+	@GetMapping("/member/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "index";
 	}
 }
